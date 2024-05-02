41d5c60feb27d9fff7cb47b20eb0985e58261968
Michael Howard <mikehow@microsoft.com>
2024-05-01 22:23:56 -0500

    Simplified hashing

--- AkvSignTest/Program.cs
+++ AkvSignTest/Program.cs
@@ -4,19 +4,23 @@ using Azure.Security.KeyVault.Keys;
 using Azure.Security.KeyVault.Keys.Cryptography;
 using System.Security.Cryptography;
 
-string keyVaultUrl = "https://xyzzy.vault.azure.net/";
-string keyName = "key-sign-xyzzy-tst";
-string dataToSign = "Hello, World!";
-
-var client = new KeyClient(new Uri(keyVaultUrl), new AzureCliCredential());
-KeyVaultKey key = client.GetKey(keyName);
-var cryptoClient = new CryptographyClient(key.Id, new AzureCliCredential());
-byte[] dataBytes = Encoding.UTF8.GetBytes(dataToSign);
-
-using (var hasher = SHA256.Create())
-{
-    byte[] hashedData = hasher.ComputeHash(dataBytes);
-    SignResult signResult = cryptoClient.Sign(SignatureAlgorithm.RS256, hashedData);
-
-    Console.WriteLine(Convert.ToBase64String(signResult.Signature));
+string? keyVaultUrl = Environment.GetEnvironmentVariable("AKV_SIGNING_URI");
+if (keyVaultUrl == null) {
+    Console.WriteLine("Please set the AkvSigningUri environment variable.");
+    return;
 }
+
+string keyName = "key-sign-mikehow-tst";
+string dataToSign = "Hello, AKV Digital Signature World!";
+
+var cred = new AzureCliCredential();
+var client = new KeyClient(new Uri(keyVaultUrl), cred);
+KeyVaultKey key = client.GetKey(keyName);
+
+var cryptoClient = new CryptographyClient(key.Id, cred);
+var dataBytes = Encoding.UTF8.GetBytes(dataToSign);
+
+var hashedData = SHA256.HashData(dataBytes);
+SignResult signResult = cryptoClient.Sign(SignatureAlgorithm.RS256, hashedData);
+
+Console.WriteLine(Convert.ToBase64String(signResult.Signature));
